433f0cec710b96bd557e4b60c7b6e683643c0915
lilaiguernelala <iguernelala@et.esiea.fr>
2025-01-07 13:11:33 +0100

    all

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ language = 'fr'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = []
+html_static_path = ['_static']
 
 source_suffix = {
     '.rst': 'restructuredtext',
